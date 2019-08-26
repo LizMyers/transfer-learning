a2ff724602f3dd0e00e13d5f3591b56f8050d0e0
LizMyers <liz@myersdesign.com>
2019-08-26 11:14:37 +0200

    Removed dead code

--- main.js
+++ main.js
@@ -20,20 +20,6 @@ import * as knnClassifier from '@tensorflow-models/knn-classifier';
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-/*------------
-// Change the background color of a class.
-$('.class').style.background = "#BADA55";
-
-// Change the inner HTML of an ID.
-$('#id').innerHTML = "<span>Cool beans!</span>";
-
-// Select all images on the webpage.
-$$('img')
-
-// Print the image addresses for all the images on a webpage.
-$$('img').forEach(img => console.log(img.src))
--------------*/
-
 // Number of classes to classify
 const NUM_CLASSES = 4;
 const labelsArr = ["Play", "Pause", "Vol 100%", "Mute"];
